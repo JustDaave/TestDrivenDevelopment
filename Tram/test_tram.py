3f3b4a20f560d9e3434d5243b93261ed971d2f39
David Dipalo-Gross <cheetahh2019@gmail.com>
2026-09-13 17:57:56 -0600

    add more tests tram

--- Tram/test_tram.py
+++ Tram/test_tram.py
@@ -160,3 +160,143 @@ def describe_stopping_at_stations():
 
         assert tram.in_motion is False
         assert tram.door_state == "open"
+
+
+def describe_departing_a_station():
+    def test_doors_are_closed_and_locked_before_departure():
+        tram = Tram(location=NORTH, direction="south")
+
+        tram.depart()
+
+        assert tram.door_state == "locked"
+        assert tram.in_motion is True
+
+
+def describe_opening_locked_doors():
+    def test_open_button_cannot_open_locked_doors():
+        tram = Tram()
+        tram.close()
+        tram.lock()
+
+        tram.open()
+
+        assert tram.door_state == "locked"
+
+    def test_forcing_locked_doors_open_causes_an_emergency():
+        tram = Tram()
+        tram.close()
+        tram.lock()
+
+        tram.force_open()
+
+        assert tram.door_state == "open"
+        assert tram.emergency_state is True
+
+
+def describe_main_console_reset():
+    def test_reset_button_is_on_the_main_console():
+        tram = Tram()
+
+        assert callable(tram.main_console.reset)
+
+    def test_reset_clears_emergency_after_doors_are_manually_closed_and_moves_to_next_station():
+        tram = Tram(location=400, direction="south")
+        tram.emergency()
+
+        tram.reset()
+        tram.close()
+
+        assert tram.emergency_state is False
+        assert tram.getLocation() == MIDDLE
+
+
+def describe_configurable_stops():
+    def test_list_of_stops_can_be_configured():
+        tram = Tram(stops=[1, 250, 750, 1000])
+
+        assert tram.stops == [1, 250, 750, 1000]
+
+
+def describe_arriving_and_departing():
+    def test_arrive_and_depart_control_the_trams_speed():
+        tram = Tram()
+
+        tram.depart()
+        assert tram.intended_speed > 0
+
+        tram.arrive()
+        assert tram.intended_speed == 0
+
+    def test_stop_arrival_message_plays_when_the_tram_arrives():
+        messages = {MIDDLE: "Welcome to the middle station"}
+        tram = Tram(location=NORTH, direction="south", arrival_messages=messages)
+
+        tram.move()
+
+        assert tram.message == "Welcome to the middle station"
+
+
+def describe_brakes():
+    def test_brakes_engage_when_intended_speed_is_less_than_current_speed():
+        tram = Tram()
+        tram.current_speed = 40
+        tram.intended_speed = 10
+
+        tram.update_brakes()
+
+        assert tram.brakes is True
+
+    def test_emergency_state_engages_the_brakes():
+        tram = Tram()
+
+        tram.emergency()
+
+        assert tram.brakes is True
+
+    def test_reset_state_tests_the_brakes():
+        tram = Tram()
+        tram.emergency()
+
+        tram.reset()
+
+        assert tram.brake_tested is True
+
+
+def describe_remote_emergency():
+    def test_emergency_can_be_engaged_remotely():
+        tram = Tram()
+
+        tram.remote_emergency()
+
+        assert tram.emergency_state is True
+        assert tram.in_motion is False
+
+
+def describe_operator_console():
+    def test_operator_can_manually_stop_and_start_the_tram():
+        tram = Tram()
+        tram.close()
+        tram.lock()
+
+        tram.main_console.start()
+        assert tram.in_motion is True
+
+        tram.main_console.stop()
+        assert tram.in_motion is False
+
+    def test_operator_can_open_and_close_the_doors():
+        tram = Tram()
+
+        tram.main_console.close_doors()
+        assert tram.door_state == "closed"
+
+        tram.main_console.open_doors()
+        assert tram.door_state == "open"
+
+    def test_operator_can_reset_the_system():
+        tram = Tram()
+        tram.emergency()
+
+        tram.main_console.reset()
+
+        assert tram.emergency_state is False
